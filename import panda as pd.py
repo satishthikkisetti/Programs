5be830aeff1e0e43899d69a2eb7677879894ef34
satishthikkisetti <satishthikkisetti@gmail.com>
2025-11-05 22:36:46 +0530

    Delete import panda as pd.py

--- import panda as pd.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-data=['steve','35','male','3.5']
-series= pd.Series(data)
-print(series)
-
-data=['steve','35','male','3.5']
-series= pd.Series(data,index=['Name','Age','gender','rating'])
-print(series)
